e53f7896239597a48485693d2b8f6b6f928c466b
Christian Robertson <robertsonc@google.com>
2014-05-29 18:07:55 -0700

    Added a bold weight for Mono

--- scripts/build-monoV2.py
+++ scripts/build-monoV2.py
@@ -8,9 +8,12 @@ from fontbuild.mix import Mix,Master
 # Masters
 
 mn = Master("%s/src/monoV2/Roboto_Mono.vfb"%BASEDIR)
+bd = Master("%s/src/monoV2/RobotoMono_Bold.vfb"%BASEDIR)
 proj = FontProject(mn.font, BASEDIR, "res/roboto.cfg", mn.ffont)
 proj.generateFont(mn.font,"Roboto Mono/Regular/Regular/Rg", kern=False)
 proj.generateFont(mn.font,"Roboto Mono/Italic/Italic/Rg", italic=True, stemWidth=180, kern=False, swapSuffixes=[".it"])
+proj.generateFont(bd.font,"Roboto Mono/Bold/Bold/Rg", kern=False)
+proj.generateFont(bd.font,"Roboto Mono/Bold Italic/Bold Italic/Rg", italic=True, stemWidth=220, kern=False, swapSuffixes=[".it"])
 
 
 

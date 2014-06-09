e3910b34373c7068a47b85bcd6755a472440c45c
Roozbeh Pournader <roozbeh@google.com>
2014-06-09 12:48:47 -0700

    Make the build run on Windows.

---
+++ build.bat
@@ -0,0 +1,8 @@
+# Modify this to point to local Fontlab executable
+set FONTLAB="C:\Program Files (x86)\Fontlab\Studio 5\Studio 5.exe"
+
+echo BASEDIR=r"%cd%" > tmp-makefontsB.flw
+type scripts\build-v2.py >> tmp-makefontsB.flw
+set PYTHONPATH=%cd%\scripts\lib
+%FONTLAB% tmp-makefontsB.flw
+del tmp-makefontsB.flw

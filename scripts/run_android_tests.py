b8f40a25fba147784ae07ed605ed98cc34049345
Roozbeh Pournader <roozbeh@google.com>
2014-10-23 20:40:34 -0700

    Move test for lack of unassigned characters from Android to general tests.

--- scripts/run_android_tests.py
+++ scripts/run_android_tests.py
@@ -49,14 +49,6 @@ class TestCharacterCoverage(unittest.TestCase):
             self.assertNotIn(0x2191, charset)  # UPWARDS ARROW
             self.assertNotIn(0x2193, charset)  # DOWNWARDS ARROW
 
-    def test_lack_of_unassigned_chars(self):
-        """Tests that unassigned characters are not in the fonts."""
-        for font in self.fonts:
-            charset = coverage.character_set(font)
-            self.assertNotIn(0x2072, charset)
-            self.assertNotIn(0x2073, charset)
-            self.assertNotIn(0x208F, charset)
-
     def test_inclusion_of_legacy_pua(self):
         """Tests that legacy PUA characters remain in the fonts."""
         for font in self.fonts:

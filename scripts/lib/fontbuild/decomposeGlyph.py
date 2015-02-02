c58f90961ecf481184a10321149183c595d83bbc
James Godfrey-Kittle <jamesgk@google.com>
2015-02-02 14:37:29 -0800

    Add decompose glyph function

--- scripts/lib/fontbuild/decomposeGlyph.py
+++ scripts/lib/fontbuild/decomposeGlyph.py
@@ -1,3 +1,12 @@
 def decomposeGlyph(glyph):
-    #TODO(jamesgk@google.com)
-    pass
+    """Moves the components of a glyph to its outline."""
+
+    font = glyph.getParent()
+    for component in glyph.components:
+        componentGlyph = font[component.baseGlyph]
+        for contour in componentGlyph:
+            contour = contour.copy()
+            contour.move(component.offset)
+            contour.scale(component.scale)
+            glyph.appendContour(contour)
+    glyph.clear(contours=False, anchors=False, guides=False)

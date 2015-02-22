77ed353f31e95f3a4e5eb18e1f0244e7968cf244
James Godfrey-Kittle <jamesgk@google.com>
2015-02-22 12:57:14 -0800

    Move after scaling during decomposition.

--- scripts/lib/fontbuild/decomposeGlyph.py
+++ scripts/lib/fontbuild/decomposeGlyph.py
@@ -6,7 +6,7 @@ def decomposeGlyph(glyph):
         componentGlyph = font[component.baseGlyph]
         for contour in componentGlyph:
             contour = contour.copy()
-            contour.move(component.offset)
             contour.scale(component.scale)
+            contour.move(component.offset)
             glyph.appendContour(contour)
     glyph.clear(contours=False, anchors=False, guides=False)

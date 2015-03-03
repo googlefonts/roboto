3476260690566a7497a25d505d55e30fd244722b
James Godfrey-Kittle <jamesgk@google.com>
2015-03-02 18:19:52 -0800

    Split up glyphs with more than one unicode value.
    This is necessary for OTF generation because ufo2fdk uses FontTools to compile cmaps, which uses a reverse lookup table of glyph names to unicode values.

--- scripts/lib/fontbuild/saveOTF.py
+++ scripts/lib/fontbuild/saveOTF.py
@@ -1,8 +1,17 @@
-from ufo2fdk import OTFCompiler
-
-
 def saveOTF(font, destFile, autohint=False):
     """Save a RoboFab font as an OTF binary using ufo2fdk."""
+
+    from ufo2fdk import OTFCompiler
+
+    # glyphs with multiple unicode values must be split up, due to FontTool's
+    # use of a name -> UV dictionary during cmap compilation
+    for glyph in font:
+        if len(glyph.unicodes) > 1:
+            newUV = glyph.unicodes.pop()
+            newGlyph = font.newGlyph("uni%04X" % newUV)
+            newGlyph.appendComponent(glyph.name)
+            newGlyph.unicode = newUV
+            newGlyph.width = glyph.width
 
     compiler = OTFCompiler()
     reports = compiler.compile(font, destFile, autohint=autohint)

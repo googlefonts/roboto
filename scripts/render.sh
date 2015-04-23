1479164b3de1b58f4eec0e71b943562aaeb60ca5
Roozbeh Pournader <roozbeh@google.com>
2015-04-22 19:24:42 -0700

    Modify render.sh to display text in all font instances.
    The input is also now read from a text file.

--- scripts/render.sh
+++ scripts/render.sh
@@ -14,15 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 HARFBUZZ=$HOME/harfbuzz
 
+input_file=$1
+
 function render {
- echo weT͟Hər | $HARFBUZZ/util/hb-view --font-file=$1 --output-format=png --output-file=$2.png
+ cat $input_file | $HARFBUZZ/util/hb-view --font-file=$1 --output-format=png --output-file=$2.png
 }
 
-render ../out/RobotoTTF/Roboto-Regular.ttf original
-render Roboto-Regular.ttf modified
+render ../hinted/Roboto-Thin.ttf 100
+render ../hinted/Roboto-Light.ttf 300
+render ../hinted/Roboto-Regular.ttf 400
+render ../hinted/Roboto-Medium.ttf 500
+render ../hinted/Roboto-Bold.ttf 700
+render ../hinted/Roboto-Black.ttf 900
+
+render ../hinted/Roboto-ThinItalic.ttf i100
+render ../hinted/Roboto-LightItalic.ttf i300
+render ../hinted/Roboto-Italic.ttf i400
+render ../hinted/Roboto-MediumItalic.ttf i500
+render ../hinted/Roboto-BoldItalic.ttf i700
+render ../hinted/Roboto-BlackItalic.ttf i900
+
+render ../hinted/RobotoCondensed-Light.ttf c300
+render ../hinted/RobotoCondensed-Regular.ttf c400
+render ../hinted/RobotoCondensed-Bold.ttf c700
+
+render ../hinted/RobotoCondensed-LightItalic.ttf ci300
+render ../hinted/RobotoCondensed-Italic.ttf ci400
+render ../hinted/RobotoCondensed-BoldItalic.ttf ci700
 
 eog *.png
 

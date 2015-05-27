c1fbee1a4bfe25aec8c0c4c4bceaa382825da95a
Roozbeh Pournader <roozbeh@google.com>
2015-05-27 09:19:29 -0700

    Remove old Windows build file.

--- build.bat
+++
@@ -1,8 +0,0 @@
-# Modify this to point to local Python executable
-set PYTHONEXE="C:\Python27\Python.exe"
-
-echo BASEDIR=r"%cd%" > tmp-makefontsB.py
-type scripts\build-v2.py >> tmp-makefontsB.py
-set PYTHONPATH=%cd%\scripts\lib
-%PYTHONEXE% tmp-makefontsB.py
-del tmp-makefontsB.py

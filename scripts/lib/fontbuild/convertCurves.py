4352446af683e05daefe67fbc8b4b525276d09ea
James Godfrey-Kittle <jamesgk@google.com>
2015-06-10 14:25:44 -0700

    Use a nicer hack for approximating cubic curves.
    To convert cubic curves to quadratic splines, we split the cubic curve in half
    and then approximate each half (which is itself a cubic curve) with a single
    quadratic curve.
    
    For this approximation we previously used the intersection of the cubic curve's
    control vectors as the second point in its resulting quadratic curve, which
    sometimes exploded into wildly inaccurate approximations. This new approximation
    method is more stable.

--- scripts/lib/fontbuild/convertCurves.py
+++ scripts/lib/fontbuild/convertCurves.py
@@ -31,22 +31,6 @@ def replaceSegments(contour, segments):
     for s in segments:
         contour.appendSegment(s.type, [(p.x, p.y) for p in s.points], s.smooth)
     
-def calcIntersect(a,b,c,d):
-    numpy.seterr(all='raise')
-    e = b-a
-    f = d-c
-    p = array([-e[1], e[0]])
-    try:
-        h = dot((a-c),p) / dot(f,p)
-    except:
-        print a,b,c,d
-        raise
-    return c + dot(f,h)
-
-def simpleConvertToQuadratic(p0,p1,p2,p3):
-    p = [array(i.x,i.y) for i in [p0,p1,p2,p3]]
-    off = calcIntersect(p[0],p[1],p[2],p[3])
-
 # OFFCURVE_VECTOR_CORRECTION = -.015
 OFFCURVE_VECTOR_CORRECTION = 0
 
@@ -62,11 +46,10 @@ def convertToQuadratic(p0,p1,p2,p3):
     pts2 = [array([i[0], i[1]]) for i in seg2]
     on1 = seg1[0]
     on2 = seg2[3]
-    try:
-        off1 = calcIntersect(pts1[0], pts1[1], pts1[2], pts1[3])
-        off2 = calcIntersect(pts2[0], pts2[1], pts2[2], pts2[3])
-    except:
-        return (p[0],p[1],p[2],p[3])
+    off1 = ((pts1[0] + (pts1[1] - pts1[0]) * 1.5) * 0.5 +
+            (pts1[3] + (pts1[2] - pts1[3]) * 1.5) * 0.5)
+    off2 = ((pts2[0] + (pts2[1] - pts2[0]) * 1.5) * 0.5 +
+            (pts2[3] + (pts2[2] - pts2[3]) * 1.5) * 0.5)
     off1 = (on1 - off1) * OFFCURVE_VECTOR_CORRECTION + off1
     off2 = (on2 - off2) * OFFCURVE_VECTOR_CORRECTION + off2
     return (on1,off1,off2,on2)

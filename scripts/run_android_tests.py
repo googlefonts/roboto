googlefonts/roboto

commit 56ad4064e8eeff6c320952daef8767188ae7fa63

Update scripts based on 03/31 delivery.

scripts/touchup_for_android.py
@@ -1,29 +1,172 @@
 #!/usr/bin/python
 """Post-build changes for Roboto for Android."""
 
+import collections
+import os
+from os import path
 import sys
 
 from fontTools import ttLib
 from nototools import font_data
+from nototools import unicode_data
 
-import temporary_touchups
+
+def drop_lookup(table, lookup_number):
+    """Drop a lookup from an OpenType table by number.
+
+    Actually remove pointers from features to the lookup, which should be less
+    intrusive.
+    """
+    for feature in table.table.FeatureList.FeatureRecord:
+        if lookup_number in feature.Feature.LookupListIndex:
+            feature.Feature.LookupListIndex.remove(lookup_number)
+            feature.Feature.LookupCount -= 1
+
+
+def get_font_name(font):
+    """Gets the name of the font from the name table."""
+    return font_data.get_name_records(font)[4]
+
+
+DIGITS = ['zero', 'one', 'two', 'three', 'four',
+          'five', 'six', 'seven', 'eight', 'nine']
+
+def fix_digit_widths(font):
+    """Change all digit widths in the font to be the same."""
+    hmtx_table = font['hmtx']
+    widths = [hmtx_table[digit][0] for digit in DIGITS]
+    if len(set(widths)) > 1:
+        width_counter = collections.Counter(widths)
+        most_common_width = width_counter.most_common(1)[0][0]
+        print 'Digit widths were %s.' % repr(widths)
+        print 'Setting all glyph widths to %d.' % most_common_width
+        for digit in DIGITS:
+            assert abs(hmtx_table[digit][0] - most_common_width) <= 1
+            hmtx_table[digit][0] = most_common_width
+
+
+_MAP_SPACING_TO_COMBINING = {
+    'acute': 'acutecomb',
+    'breve': 'brevenosp',
+    'caron': 'uni030C',
+    'cedilla': 'cedillanosp',
+    'circumflex': 'circumflexnosp',
+    'dieresis': 'dieresisnosp',
+    'dotaccent': 'dotnosp',
+    'grave': 'gravecomb',
+    'hungarumlaut': 'acutedblnosp',
+    'macron': 'macroncomb',
+    'ogonek': 'ogoneknosp',
+    'tilde': 'tildecomb',
+    'ring': 'ringnosp',
+    'tonos': 'acutecomb',
+    'uni02F3': 'ringsubnosp',
+}
+
+def fix_ccmp_lookup(font):
+    """Fixes the broken ccmp lookup."""
+    cmap = font_data.get_cmap(font)
+    reverse_cmap = {name: code for (code, name) in cmap.items()}
+
+    # Where we know the bad 'ccmp' is
+    ccmp_lookup = font['GSUB'].table.LookupList.Lookup[2]
+    assert ccmp_lookup.LookupType == 4
+    assert ccmp_lookup.SubTableCount == 1
+    ligatures = ccmp_lookup.SubTable[0].ligatures
+    for first_char, ligtable in ligatures.iteritems():
+        ligatures_to_delete = []
+        for index, ligature in enumerate(ligtable):
+            assert len(ligature.Component) == 1
+            component = ligature.Component[0]
+            if (component.endswith('comb')
+                or component in ['commaaccent',
+                                 'commaaccentrotate',
+                                 'ringacute']):
+                continue
+            # https://code.google.com/a/google.com/p/roboto/issues/detail?id=54
+            if first_char == 'a' and component == 'uni02BE':
+                ligatures_to_delete.append(index)
+                continue
+            char = reverse_cmap[component]
+            general_category = unicode_data.category(char)
+            if general_category != 'Mn': # not a combining mark
+                ligature.Component[0] = _MAP_SPACING_TO_COMBINING[component]
+        ligatures[first_char] = [
+            ligature for (index, ligature) in enumerate(ligtable)
+            if index not in ligatures_to_delete]
+
+
+def apply_temporary_fixes(font):
+    """Apply some temporary fixes.
+    """
+    # Make sure macStyle is correct
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=8
+    font_name = get_font_name(font)
+    bold = ('Bold' in font_name) or ('Black' in font_name)
+    italic = 'Italic' in font_name
+    font['head'].macStyle = (italic << 1) | bold
+
+    # Mark the font free for installation, embedding, etc.
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=29
+    os2 = font['OS/2']
+    os2.fsType = 0
+
+    # Set the font vendor to Google
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=46
+    os2.achVendID = 'GOOG'
+
+    # Drop the lookup forming the ff ligature
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=47
+    drop_lookup(font['GSUB'], 5)
+
+    # Correct the ccmp lookup to use combining marks instead of spacing ones
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=48
+    fix_ccmp_lookup(font)
+
+    # Fix the digit widths
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=49
+    fix_digit_widths(font)
+
+    # Add cmap for U+2117 SOUND RECORDING COPYRIGHT
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=44
+    font_data.add_to_cmap(font, {0x2117: 'published'})
+
+    # Fix version number from buildnumber.txt
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=50
+    from datetime import date
+
+    build_number_txt = path.join(
+        path.dirname(__file__), os.pardir, 'res', 'buildnumber.txt')
+    build_number = open(build_number_txt).read().strip()
+
+    version_record = 'Version 2.0%s; %d' % (build_number, date.today().year)
+
+    for record in font['name'].names:
+        if record.nameID == 5:
+            if record.platformID == 1 and record.platEncID == 0:  # MacRoman
+                record.string = version_record
+            elif record.platformID == 3 and record.platEncID == 1:
+                # Windows UCS-2
+                record.string = version_record.encode('UTF-16BE')
+            else:
+                assert False
 
 
 def apply_android_specific_fixes(font):
     """Apply fixes needed for Android."""
-    # Remove tab, combining keycap, and the arrows from the cmap table.
-    #
-    # Arrows are removed to maximize consistency of arrows, since the rest
-    # of the arrows come from Noto Symbols.
-    #
-    # And here are the bugs for the other two issues:
+    # Set ascent, descent, and lineGap values to Android K values
+    hhea = font['hhea']
+    hhea.ascent = 1900
+    hhea.descent = -500
+    hhea.lineGap = 0
+
+    # Remove tab, combining keycap, the arrows, and unassigned characters
+    # from the cmap table
     # https://code.google.com/a/google.com/p/roboto/issues/detail?id=51
     # https://code.google.com/a/google.com/p/roboto/issues/detail?id=52
+    # https://code.google.com/a/google.com/p/roboto/issues/detail?id=53
     font_data.delete_from_cmap(font, [
-        0x0009, # tab
-        0x20E3, # combining keycap
-        0x2191, 0x2193, # vertical arrows
-    ])
+        0x0009, 0x20E3, 0x2072, 0x2073, 0x208F, 0x2191, 0x2193])
 
     # Drop tables not useful on Android
     for table in ['LTSH', 'hdmx', 'VDMX', 'gasp']:
@@ -34,8 +177,7 @@ def apply_android_specific_fixes(font):
 def correct_font(source_font_name, target_font_name):
     """Corrects metrics and other meta information."""
     font = ttLib.TTFont(source_font_name)
-    temporary_touchups.apply_temporary_fixes(font)
-    temporary_touchups.update_version_and_revision(font)
+    apply_temporary_fixes(font)
     apply_android_specific_fixes(font)
     font.save(target_font_name)
 

scripts/run_android_tests.py
@@ -1,53 +1,152 @@
 #!/usr/bin/python
 """Test assumptions that Android relies on."""
 
+import glob
+import json
 import unittest
 
+from fontTools import ttLib
 from nototools import coverage
-
-import common_tests
-
-FONTS = common_tests.load_fonts(
-    ['out/v2/android/*.ttf'],
-    expected_count=18)
-
-class TestItalicAngle(common_tests.TestItalicAngle):
-    loaded_fonts = FONTS
+from nototools import font_data
+from nototools import render
+from nototools import unicode_data
 
 
-class TestMetaInfo(common_tests.TestMetaInfo):
-    loaded_fonts = FONTS
+def load_fonts():
+    """Load all fonts built for Android."""
+    all_font_files = glob.glob('out/android/*.ttf')
+    all_fonts = [ttLib.TTFont(font) for font in all_font_files]
+    assert len(all_font_files) == 18
+    return all_font_files, all_fonts
 
 
-class TestNames(common_tests.TestNames):
-    loaded_fonts = FONTS
-    family_name = 'Roboto'
+class TestVerticalMetrics(unittest.TestCase):
+    """Test the vertical metrics of fonts."""
+
+    def setUp(self):
+        _, self.fonts = load_fonts()
+
+    def test_ymin_ymax(self):
+        """Tests yMin and yMax to be equal to what Android expects."""
+        for font in self.fonts:
+            head_table = font['head']
+            self.assertEqual(head_table.yMin, -555)
+            self.assertEqual(head_table.yMax, 2163)
 
 
-class TestDigitWidths(common_tests.TestDigitWidths):
-    loaded_fonts = FONTS
+class TestDigitWidths(unittest.TestCase):
+    """Tests the width of digits."""
+
+    def setUp(self):
+        _, self.fonts = load_fonts()
+        self.digits = [
+            'zero', 'one', 'two', 'three', 'four',
+            'five', 'six', 'seven', 'eight', 'nine']
+
+    def test_digit_widths(self):
+        """Tests all decimal digits to make sure they have the same width."""
+        for font in self.fonts:
+            hmtx_table = font['hmtx']
+            widths = [hmtx_table[digit][0] for digit in self.digits]
+            self.assertEqual(len(set(widths)), 1)
 
 
-class TestCharacterCoverage(common_tests.TestCharacterCoverage):
-    loaded_fonts = FONTS
+class TestCharacterCoverage(unittest.TestCase):
+    """Tests character coverage."""
+
+    def setUp(self):
+        _, self.fonts = load_fonts()
+        self.LEGACY_PUA = frozenset({0xEE01, 0xEE02, 0xF6C3})
 
     def test_lack_of_arrows_and_combining_keycap(self):
-        """Tests that arrows and combining keycap are not in Android fonts."""
+        """Tests that arrows and combining keycap are not in the fonts."""
         for font in self.fonts:
             charset = coverage.character_set(font)
             self.assertNotIn(0x20E3, charset)  # COMBINING ENCLOSING KEYCAP
             self.assertNotIn(0x2191, charset)  # UPWARDS ARROW
             self.assertNotIn(0x2193, charset)  # DOWNWARDS ARROW
 
+    def test_lack_of_unassigned_chars(self):
+        """Tests that unassigned characters are not in the fonts."""
+        for font in self.fonts:
+            charset = coverage.character_set(font)
+            self.assertNotIn(0x2072, charset)
+            self.assertNotIn(0x2073, charset)
+            self.assertNotIn(0x208F, charset)
 
-class TestLigatures(common_tests.TestLigatures):
-    loaded_fonts = FONTS
+    def test_inclusion_of_sound_recording_copyright(self):
+        """Tests that sound recording copyright symbol is in the fonts."""
+        for font in self.fonts:
+            charset = coverage.character_set(font)
+            self.assertIn(
+                0x2117, charset,  # SOUND RECORDING COPYRIGHT
+                'U+2117 not found in %s.' % font_data.font_name(font))
+
+    def test_inclusion_of_legacy_pua(self):
+        """Tests that legacy PUA characters remain in the fonts."""
+        for font in self.fonts:
+            charset = coverage.character_set(font)
+            for char in self.LEGACY_PUA:
+                self.assertIn(char, charset)
+
+    def test_non_inclusion_of_other_pua(self):
+        """Tests that there are not other PUA characters except legacy ones."""
+        for font in self.fonts:
+            charset = coverage.character_set(font)
+            pua_chars = {
+                char for char in charset
+                if 0xE000 <= char <= 0xF8FF or 0xF0000 <= char <= 0x10FFFF}
+            self.assertTrue(pua_chars <= self.LEGACY_PUA)
 
 
-class TestVerticalMetrics(common_tests.TestVerticalMetrics):
-    loaded_fonts = FONTS
+class TestSpacingMarks(unittest.TestCase):
+    """Tests that spacing marks are indeed spacing."""
+
+    def setUp(self):
+        self.font_files, _ = load_fonts()
+        charset = coverage.character_set(self.font_files[0])
+        self.marks_to_test = [char for char in charset
+                              if unicode_data.category(char) in ['Lm', 'Sk']]
+        self.advance_cache = {}
+
+    def get_advances(self, text, font):
+        """Get a list of horizontal advances for text rendered in a font."""
+        try:
+            return self.advance_cache[(text, font)]
+        except KeyError:
+            hb_output = render.run_harfbuzz_on_text(text, font, '')
+            hb_output = json.loads(hb_output)
+            advances = [glyph['ax'] for glyph in hb_output]
+            self.advance_cache[(text, font)] = advances
+            return advances
+
+    def test_individual_spacing_marks(self):
+        """Tests that spacing marks are spacing by themselves."""
+        for font in self.font_files:
+            print 'Testing %s for stand-alone spacing marks...' % font
+            for mark in self.marks_to_test:
+                mark = unichr(mark)
+                advances = self.get_advances(mark, font)
+                assert len(advances) == 1
+                self.assertNotEqual(advances[0], 0)
+
+    def test_spacing_marks_in_combination(self):
+        """Tests that spacing marks do not combine with base letters."""
+        for font in self.font_files:
+            print 'Testing %s for spacing marks in combination...' % font
+            for base_letter in (u'A\u00C6BCDEFGHIJKLMNO\u00D8\u01A0PRST'
+                                u'U\u01AFVWXYZ'
+                                u'a\u00E6bcdefghi\u0131j\u0237klmn'
+                                u'o\u00F8\u01A1prs\u017Ftu\u01B0vwxyz'
+                                u'\u03D2'):
+                print 'Testing %s combinations' % base_letter
+                for mark in self.marks_to_test:
+                    mark = unichr(mark)
+                    advances = self.get_advances(base_letter + mark, font)
+                    self.assertEqual(len(advances), 2,
+                        'The sequence <%04X, %04X> combines, '
+                        'but it should not' % (ord(base_letter), ord(mark)))
 
 
 if __name__ == '__main__':
     unittest.main()
-

scripts/build-v2.py
@@ -49,7 +49,8 @@ def condenseFont(font, scale=.8, stemWidth=185):
         m = Transform(xscale, 0, 0, 1, 20, 0)
         g.transform(m)
         transformFLGlyphMembers(g,m,transformAnchors=False)
-        g.width += 40
+        if g.width != 0:
+            g.width += 40
     return f
 
 
@@ -75,7 +76,7 @@ proj.generateFont(Mix([rg,bd], RPoint(1.125, 1.0)),"%s/Black/Bold/Bk"%FAMILYNAME
 proj.generateFont(th.font,"%s/Thin Italic/Italic/Th"%FAMILYNAME, italic=True, stemWidth=80)
 proj.generateFont(Mix([th,rg], 0.45),"%s/Light Italic/Italic/Lt"%FAMILYNAME, italic=True, stemWidth=120)
 proj.generateFont(Mix([th,rg], RPoint(0.90, 0.92)),"%s/Italic/Italic/Rg"%FAMILYNAME, italic=True, stemWidth=185)
-proj.generateFont(Mix([rg,bd], 0.35),"%s/Medium Italic/Bold Italic/Lt"%FAMILYNAME, italic=True, stemWidth=230)
+proj.generateFont(Mix([rg,bd], 0.35),"%s/Medium Italic/Italic/Lt"%FAMILYNAME, italic=True, stemWidth=230)
 proj.generateFont(Mix([rg,bd], RPoint(0.73, 0.73)),"%s/Bold Italic/Bold Italic/Rg"%FAMILYNAME, italic=True, stemWidth=290)
 proj.generateFont(Mix([rg,bd], RPoint(1.125, 1.0)),"%s/Black Italic/Bold Italic/Bk"%FAMILYNAME, italic=True, stemWidth=290)
 

scripts/force_yminmax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Post-subset changes for Roboto."""
+"""Post-subset changes for Roboto for Android."""
 
 import sys
 

scripts/lib/fontbuild/Build.py
@@ -29,7 +29,7 @@ class FontProject:
         self.diacriticList = [line.strip() for line in diacriticList if not line.startswith("#")]
         self.ot_classes = open(self.basedir + "/" + self.config.get("res","ot_classesfile")).read()
         self.ot_kerningclasses = open(self.basedir + "/" + self.config.get("res","ot_kerningclassesfile")).read()
-        self.ot_features = open(self.basedir + "/" + self.config.get("res","ot_featuresfile")).read()
+        #self.ot_features = open(self.basedir + "/" + self.config.get("res","ot_featuresfile")).read()
         adobeGlyphList = open(self.basedir + "/" + self.config.get("res", "agl_glyphlistfile")).readlines()
         self.adobeGlyphList = dict([line.split(";") for line in adobeGlyphList if not line.startswith("#")])
         
@@ -126,7 +126,8 @@ class FontProject:
                 elif g.name != ".notdef":
                     italicizeGlyph(f, g, 10, stemWidth=stemWidth)
                 removeGlyphOverlap(g)
-                g.width += 10
+                if g.width != 0:
+                    g.width += 10
 
         if swapSuffixes != None:
             for swap in swapSuffixes:

scripts/lib/fontbuild/anchors.py
@@ -13,6 +13,37 @@ def getAnchorByName(g,anchorName):
             return a
 
 
+def moveMarkAnchors(f, g, anchorName, accentName, dx, dy):
+    if "top"==anchorName:
+        anchors = f[accentName].anchors
+        for anchor in anchors:
+            if "mkmktop_acc" == anchor.name:
+                anchor2 = Anchor()
+                #print anchor.x, dx, anchor.y, dy
+                anchor2.name = "top"
+                anchor2.x = anchor.x + int(dx)
+                anchor2.y = anchor.y + int(dy)
+                g.anchors.append(anchor2)
+ 
+    elif "bottom"==anchorName:
+        anchors = f[accentName].anchors
+        for anchor in anchors:
+            if "mkmkbottom_acc" == anchor.name:
+                for n in range(len(g.anchors)):
+                    if g.anchors[n].name == "bottom":
+                        del g.anchors[n]
+                        break
+                anchor2 = Anchor()
+                #print anchor.x, dx, anchor.y, dy
+                anchor2.name = "bottom"
+                anchor2.x = anchor.x + int(dx)
+                anchor2.y = anchor.y + int(dy)
+                for anc in anchors:
+                    if "top" == anc.name:
+                        anchor2.x = anc.x + int(dx)
+                g.anchors.append(anchor2)
+
+
 def alignComponentToAnchor(f,glyphName,baseName,accentName,anchorName):
     g = getGlyph(glyphName,f)
     base = getGlyph(baseName,f)
@@ -26,6 +57,8 @@ def alignComponentToAnchor(f,glyphName,baseName,accentName,anchorName):
     offset = (a1.x - a2.x, a1.y - a2.y)
     c = getComponentByName(f, g, accentName)
     c.offset = offset
+    moveMarkAnchors(f, g, anchorName, accentName, offset.x, offset.y)
+
 
 def alignComponentsToAnchors(f,glyphName,baseName,accentNames):
     for a in accentNames:

scripts/lib/fontbuild/generateGlyph.py
@@ -16,6 +16,43 @@ def parseComposite(composite):
     return (glyphName, baseName, accentNames, offset)
 
 
+def copyMarkAnchors(f, g, srcname, width):
+    anchors = f[srcname].anchors
+    for anchor in anchors:
+        if "top_dd" == anchor.name:
+            anchor1 = Anchor(anchor)
+            anchor1.x += width
+            g.anchors.append(anchor1)
+        if "bottom_dd" == anchor.name:
+            anchor1 = Anchor(anchor)
+            anchor1.x += width
+            g.anchors.append(anchor1)
+        if "top0315" == anchor.name:
+            anchor1 = Anchor(anchor)
+            anchor1.x += width
+            g.anchors.append(anchor1)
+        if "top" == anchor.name:
+            if g.unicode == None:
+                continue
+            if g.unicode > 0x02B0:
+                continue
+            parenttop_present = 0
+            for anc in g.anchors:
+                if anc.name == "parent_top":
+                    parenttop_present = 1
+            if parenttop_present:
+                continue
+            anchor1 = Anchor(anchor)
+            anchor1.name = "parent_top"
+#            anchor1.x += width
+            g.anchors.append(anchor1)
+
+ #       if "rhotichook" == anchor.name:
+ #           anchor1 = Anchor(anchor)
+ #           anchor1.x += width
+ #           g.anchors.append(anchor1)
+
+
 def generateGlyph(f,gname,glyphList={}):
     glyphName, baseName, accentNames, offset = parseComposite(gname)
 
@@ -34,11 +71,12 @@ def generateGlyph(f,gname,glyphList={}):
                     "anchor not found in glyph '%s'" % (gname, e, baseName))
                 return
             g = f[glyphName]
-            if len(accentNames) > 0:
-                alignComponentsToAnchors(f, glyphName, baseName, accentNames)
+            copyMarkAnchors(f, g, baseName, offset[1] + offset[0])
             if offset[0] != 0 or offset[1] != 0:
                 g.width += offset[1] + offset[0]
                 g.move((offset[0], 0))
+            if len(accentNames) > 0:
+                alignComponentsToAnchors(f, glyphName, baseName, accentNames)
         else:
             print ("Existing glyph '%s' found in font, ignoring composition "
                 "rule '%s'" % (glyphName, gname))

scripts/lib/fontbuild/instanceNames.py
@@ -17,7 +17,7 @@ class InstanceNames:
     def __init__(self,names):
         if type(names) == type(" "):
             names = names.split("/")
-            
+        #print names            
         self.longfamily =      names[0]
         self.longstyle =       names[1]
         self.shortstyle =      names[2]
@@ -84,21 +84,29 @@ class InstanceNames:
         flFont.weight =           self.weight 
         flFont.weight_code =      self._getWeightCode(self.weight) 
         flFont.width =            self.width
+        if len(self.italic):
+            flFont.italic_angle = -12
 
         fn = flFont.fontnames
         fn.clean()
-        fn.append(NameRecord(0,1,0,0,     "Font data copyright %s %s" %(self.foundry, self.year) ))
-        fn.append(NameRecord(0,3,1,1033,  "Font data copyright %s %s" %(self.foundry, self.year) ))
+        #fn.append(NameRecord(0,1,0,0,     "Font data copyright %s %s" %(self.foundry, self.year) ))
+        #fn.append(NameRecord(0,3,1,1033,  "Font data copyright %s %s" %(self.foundry, self.year) ))
+        fn.append(NameRecord(0,1,0,0,     "Copyright %s %s Inc. All Rights Reserved." %(self.year, self.foundry) ))
+        fn.append(NameRecord(0,3,1,1033,  "Copyright %s %s Inc. All Rights Reserved." %(self.year, self.foundry) ))
         fn.append(NameRecord(1,1,0,0,     self.longfamily ))
         fn.append(NameRecord(1,3,1,1033,  self.shortfamily ))
         fn.append(NameRecord(2,1,0,0,     self.longstyle ))
         fn.append(NameRecord(2,3,1,1033,  self.longstyle ))
-        fn.append(NameRecord(3,1,0,0,     "%s:%s:%s" %(self.foundry, self.longfamily, self.year) ))
-        fn.append(NameRecord(3,3,1,1033,  "%s:%s:%s" %(self.foundry, self.longfamily, self.year) ))
+        #fn.append(NameRecord(3,1,0,0,     "%s:%s:%s" %(self.foundry, self.longfamily, self.year) ))
+        #fn.append(NameRecord(3,3,1,1033,  "%s:%s:%s" %(self.foundry, self.longfamily, self.year) ))
+        fn.append(NameRecord(3,1,0,0,     "%s:%s:%s" %(self.foundry, self.fullname, self.year) ))
+        fn.append(NameRecord(3,3,1,1033,  "%s:%s:%s" %(self.foundry, self.fullname, self.year) ))        
         fn.append(NameRecord(4,1,0,0,     self.fullname ))
         fn.append(NameRecord(4,3,1,1033,  self.fullname ))
-        fn.append(NameRecord(5,1,0,0,     "Version %s%s; %s" %(self.version, self.build, self.year) ))
-        fn.append(NameRecord(5,3,1,1033,  "Version %s%s; %s" %(self.version, self.build, self.year) ))
+        #fn.append(NameRecord(5,1,0,0,     "Version %s%s; %s" %(self.version, self.build, self.year) ))
+        #fn.append(NameRecord(5,3,1,1033,  "Version %s%s; %s" %(self.version, self.build, self.year) ))
+        fn.append(NameRecord(5,1,0,0,     "Version %s; %s" %(self.version, self.year) ))
+        fn.append(NameRecord(5,3,1,1033,  "Version %s; %s" %(self.version, self.year) ))
         fn.append(NameRecord(6,1,0,0,     self.postscript ))
         fn.append(NameRecord(6,3,1,1033,  self.postscript ))
         fn.append(NameRecord(7,1,0,0,     "%s is a trademark of %s." %(self.longfamily, self.foundry) ))
@@ -131,7 +139,7 @@ class InstanceNames:
         return self._getSubstyle(r"Italic|Oblique|Obliq")
 
     def _getWeight(self):
-        w = self._getSubstyle(r"Extrabold|Superbold|Super|Fat|Bold|Semibold|Demibold|Medium|Light|Thin")
+        w = self._getSubstyle(r"Extrabold|Superbold|Super|Fat|Black|Bold|Semibold|Demibold|Medium|Light|Thin")
         if w == "":
             w = "Regular"
         return w
@@ -143,6 +151,7 @@ class InstanceNames:
         return w
     
     def _getStyleCode(self):
+        #print "shortstyle:", self.shortstyle
         styleCode = 0
         if self.shortstyle == "Bold":
             styleCode = 32
@@ -166,7 +175,7 @@ class InstanceNames:
         elif weight == "Semibold":
             return 600
         elif weight == "Black":
-            return 800
+            return 900
         elif weight == "Fat":
             return 900
 

commit 438efb81c9e02228d6f733febd902c29d4974be1

Re-consolidate touchup scripts
These accidentally regressed somewhat in
56ad4064e8eeff6c320952daef8767188ae7fa63. For practical purposes all
of the significant regressions have been fixed since; this just
removes some lingering duplicate code and missing tests.

scripts/touchup_for_android.py
@@ -21,35 +21,11 @@ import sys
 from fontTools import ttLib
 from nototools import font_data
 
-import roboto_data
-
-
-def apply_temporary_fixes(font):
-    """Apply some temporary fixes."""
-
-    from datetime import date
-
-    version_number = roboto_data.get_version_number()
-    version_record = 'Version %s; %d' % (version_number, date.today().year)
-
-    for record in font['name'].names:
-        if record.nameID == 5:
-            if record.platformID == 1 and record.platEncID == 0:  # MacRoman
-                record.string = version_record
-            elif record.platformID == 3 and record.platEncID == 1:
-                # Windows UCS-2
-                record.string = version_record.encode('UTF-16BE')
-            else:
-                assert False
+import temporary_touchups
 
 
 def apply_android_specific_fixes(font):
     """Apply fixes needed for Android."""
-    # Set ascent, descent, and lineGap values to Android K values
-    hhea = font['hhea']
-    hhea.ascent = 1900
-    hhea.descent = -500
-    hhea.lineGap = 0
 
     # Remove combining keycap and the arrows from the cmap table:
     # https://github.com/google/roboto/issues/99
@@ -79,8 +55,10 @@ def apply_android_specific_fixes(font):
 
 def correct_font(source_font_name, target_font_name):
     """Corrects metrics and other meta information."""
+
     font = ttLib.TTFont(source_font_name)
-    apply_temporary_fixes(font)
+    temporary_touchups.apply_temporary_fixes(font)
+    temporary_touchups.update_version_and_revision(font)
     apply_android_specific_fixes(font)
     font.save(target_font_name)
 

scripts/run_android_tests.py
@@ -27,6 +27,10 @@ FONTS = font_tests.load_fonts(
     expected_count=18)
 
 
+class TestItalicAngle(run_general_tests.TestItalicAngle):
+    loaded_fonts = FONTS
+
+
 class TestMetaInfo(run_general_tests.TestMetaInfo):
     """Bugs:
     https://github.com/google/roboto/issues/142
@@ -47,8 +51,6 @@ class TestNames(run_general_tests.TestNames):
 
 class TestVerticalMetrics(font_tests.TestVerticalMetrics):
     loaded_fonts = FONTS
-    test_glyphs_ymin_ymax = None
-    test_hhea_table_metrics = None
     test_os2_metrics = None
 
     # tests yMin and yMax to be equal to Roboto v1 values
@@ -56,10 +58,14 @@ class TestVerticalMetrics(font_tests.TestVerticalMetrics):
     expected_head_yMin = -555
     expected_head_yMax = 2163
 
+    # test ascent, descent, and lineGap to be equal to Roboto v1 values
+    expected_hhea_descent = -500
+    expected_hhea_ascent = 1900
+    expected_hhea_lineGap = 0
+
 
 class TestDigitWidths(font_tests.TestDigitWidths):
     loaded_fonts = FONTS
-    test_superscript_digits = None
 
 
 class TestCharacterCoverage(font_tests.TestCharacterCoverage):
@@ -78,7 +84,7 @@ class TestCharacterCoverage(font_tests.TestCharacterCoverage):
         ) - include  # don't exclude legacy PUA
 
 
-class TestSpacingMarks(font_tests.TestSpacingMarks):
+class TestLigatures(font_tests.TestLigatures):
     loaded_fonts = FONTS
 
 

scripts/run_general_tests.py
@@ -54,8 +54,7 @@ class TestMetaInfo(font_tests.TestMetaInfo):
     mark_italic_as_oblique = True
     test_us_weight = None
 
-    #expected_version = '2.' + roboto_data.get_build_number()
-    test_version_numbers = None
+    expected_version = roboto_data.get_version_number()
 
     # fsType of 0 marks the font free for installation, embedding, etc.
     expected_os2_fsType = 0